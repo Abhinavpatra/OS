c84d0452dacddf574d91781eec32050ac390432d
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2025-07-05 23:04:49 +0530

    checks with jest

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-7xl">Hello world!</h1>
+      <h1 className="text-7xl">hello         world!</h1>
+      <h2>
+        not hello
+      </h2>
     </div>
 
   );
